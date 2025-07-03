afb23280a5d6a5fe27f03ab9b3b4b50c4bbd3fff
EduarGabriel10 <eduar.gabrielito100@gmail.com>
2025-07-03 02:02:34 -0500

    Actualizacion3julio2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyCa7N2VbDbE7uwBDbqk8QObPPxa81rBGQM",
-    authDomain: "proyectouic-7ca3f.firebaseapp.com",
-    projectId: "proyectouic-7ca3f",
-    storageBucket: "proyectouic-7ca3f.firebasestorage.app",
+    apiKey: "AIzaSyBiUfb2g6-i1bb-K2lD-x7NplxZhNzZnjA",
+    authDomain: "vitality1-14a51.firebaseapp.com",
+    projectId: "vitality1-14a51",
+    storageBucket: "vitality1-14a51.firebasestorage.app",
     messagingSenderId: "605761446805",
     appId: "1:605761446805:web:ce7e852daee6c411af4c9c"
   },
